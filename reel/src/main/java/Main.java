db885ccd472bba2a3701f94afba5e0cd240876d3
Andrey Grishchenko <parmalat35@live.com>
2025-06-07 21:54:19 +0300

    CI checkstyle action configured (#1)
    * Configured checkstyle mill check in github CI 
    * Checkstyle warnings severity level -> error

--- reel/src/main/java/Main.java
+++ reel/src/main/java/Main.java
@@ -1,5 +1,9 @@
+/**
+ * Main class.
+ */
 public class Main {
+    /** Main method. */
     public static void main(String[] args) {
-        System.out.println("Hello, Mill Java!");
+        System.out.println("Hello, Mill Java!\n");
     }
 }
