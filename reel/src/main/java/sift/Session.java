633d60152db5c450147fca671e1a5887e2e93ffa
GingerYouth <parmalat35@live.com>
2025-06-13 20:09:02 +0300

    Session DTO

---
+++ reel/src/main/java/sift/Session.java
@@ -0,0 +1,10 @@
+package main.java.sift;
+
+import java.time.LocalTime;
+
+/** Session DTO. */
+public record Session(
+    LocalTime dateTime, String name, String description,
+    String cinema, String address, int price, String link) {
+}
+
